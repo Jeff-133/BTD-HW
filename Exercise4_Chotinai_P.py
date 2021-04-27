b0cfeab7f42dd9d8706b4d7e1e5c85a3c7e1ef4c
Jeff-133 <Jeff.phalanchai@gmail.com>
2021-04-27 13:14:47 +0700

    Added Exercise4

---
+++ Exercise4_Chotinai_P.py
@@ -0,0 +1,13 @@
+FOU_ENG = "50"
+GEN_BUS = "52.5"
+INT_COM = "63.25"
+COM_PRO = "70"
+
+print("----------------------")
+print("------You Score-------")
+print("----------------------")
+print("Foundation English"":",FOU_ENG)
+print("General Business"":",GEN_BUS)
+print("Introduction to Computer Systems"":",INT_COM)
+print("Computer Programming"":",COM_PRO)
+print("----------------------")
